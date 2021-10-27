42a88c598c61f4024ec23c59704057493321d74c
krgneha <neha224229@gmail.com>
2021-10-28 01:59:29 +0530

    repository updated

--- 05_Weight_Conversion_Tool/kgtop.js
+++
@@ -1,5 +0,0 @@
-function weightConverter(valNum){
-    var valNum=valNum*2.2046;
-    var n=valNum.toFixed(0);
-    document.getElementById("outputPounds").innerHTML=n;
-}
---
+++ Project-7-Bouncing-Ball/js/script.js
@@ -0,0 +1,94 @@
+// this project is created by krishna6431
+
+var canvas = document.getElementById("mycanvas");
+//create 2d context from canvas
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+var paddleHeight = 12;
+var paddleWidth = 72;
+var paddleX = (canvas.width - paddleWidth) / 2;
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
+function drawBall() {
+    ctx.beginPath(); //begin path from drawing ball
+    // arc(x, y, radius, startAngle, endAngle, false / true(direction))
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2, false);
+    ctx.fillStyle = "red"; //give color to shape circle
+    ctx.fill(); // fill color to shape
+    ctx.closePath(); // close of path 
+}
+
+function drawPaddle() {
+    ctx.beginPath(); //begin path from drawing ball
+    ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "blue"; //give color to shape paddle
+    ctx.fill(); // fill color to shape
+    ctx.closePath(); // close of path 
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); //clear canvas
+    drawBall();
+    drawPaddle();
+    //check for left and right wall collision
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    //check for top wall collision
+    if (y + dy < ballRadius) {
+        dy = -dy;
+    }
+    //check for bottom ball collision
+    else if (y + dy > canvas.height - ballRadius) {
+        if (x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("Game Over!!!");
+            x = canvas.width / 2;
+            y = canvas.height - 30;
+            document.location.reload(true);
+        }
+    }
+
+
+
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 5;
+    }
+    else if (leftPressed && paddleX > 0) {
+        paddleX -= 5;
+    }
+    x += dx;
+    y += dy;
+}
+
+//infinte loop
+// setInterval(method name , time in ms)
+setInterval(draw, 10);
